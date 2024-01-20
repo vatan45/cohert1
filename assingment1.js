2952b42a4451c5be3fb49648d8b3bb581b0c4ba7
vatanmalik45 <vmro45@gmail.com>
2024-01-20 13:16:57 +0530

    this is assignment of week1

--- assingment1.js
+++ assingment1.js
@@ -27,19 +27,80 @@
 // console.log(ans);
 
 // dcba => ['d', 'c', 'b', 'a']
-function sort (str){
-    var array = str.split("")
-    array = array.sort();
-    var sortedstring = array.join("");
-    return sortedstring;
-}
-function isAnagram(str1, str2){
-    if (sort(str1) == sort(str2)){
-        return true;
-    } else {
-        return false;
+// function sort (str){
+//     var array = str.split("")
+//     array = array.sort();
+//     var sortedstring = array.join("");
+//     return sortedstring;
+// }
+// function isAnagram(str1, str2){
+//     if (sort(str1) == sort(str2)){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// var ans = isAnagram("hark", "arkh");
+// console.log(ans);
+
+function solve(transactions){
+    var spendestimates = {};
+    for(var i=0; i<transactions.length;i++){
+        var  t= transactions[i];
+        if(spendestimates[t.category]){
+            spendestimates[t.category]= spendestimates[t.category] + t.price
+        }else{
+            spendestimates[t.category]= t.price
+        }
+
+    
     }
+    console.log(spendestimates);
 }
 
-var ans = isAnagram("hark", "arkh");
-console.log(ans);
+
+
+
+
+
+
+
+
+
+var transactions = [
+    {
+    itemname: "pepsi",
+    category: "drink",
+    price: 20,
+    timestamp: 123123123
+},
+{
+    itemname: "merinda",
+    category: "drink",
+    price: 30,
+    timestamp: 123123123
+},
+{
+    itemname: "samosa",
+    category: "food",
+    price: 5,
+    timestamp: 123123123
+}
+]
+
+solve(transactions)
+
+// [
+//     {
+//         category: "drink",
+//         spent: 45
+//     },
+//     {
+//         category: "food",
+//         spent: 5
+//     }
+// ]
+
+
+
